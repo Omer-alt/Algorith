14cdbd73ead0ddece74b67ad5aa8081800848df1
Omer-alt <wafomer912@gmail.com>
2024-03-24 16:51:46 +0000

    Fix: First level bug fixed about acuracy

--- Neural_network/neural-network.py
+++ Neural_network/neural-network.py
@@ -43,32 +43,6 @@ class NeuronLayer (Neuron):
         self.dA_dZ = self.d_sigmoid(self.Z)
         self.dZ_dW = self.A.T
         
-        """ 
-        dL_dA2 = (A2-Y)/(A2*(1-A2)) #dans le reseau principale
-        dA2_dZ2 = d_sigmoid(Z2) #oui_deja
-        dZ2_dW2 = A1.T #oui_deja
-
-        dW2 = ( dL_dA2 * dA2_dZ2  ) @ dZ2_dW2 #dans le NN
-        db2 = dL_dA2 @ dA2_dZ2.T #dans le NN
-
-        dZ2_dA1 = W2
-        dA1_dZ1 = d_sigmoid(Z1) #oui
-        dZ1_dW1 = X.T #oui
-
-        dW1 = ( dZ2_dA1.T * (dL_dA2 * dA2_dZ2) * dA1_dZ1 ) @ dZ1_dW1 #dans le NN
-        db1 = ((dL_dA2 * dA2_dZ2) @ (dZ2_dA1.T * dA1_dZ1).T).T #dans le NN
-        """
-    
-    """    
-    def get_out(self, Mat):
-        self.Z =  self.W.dot(Mat) + self.b
-        
-     # deja
-    def init_params(self, h_prec, h_curr, h_suiv):
-        self.W = np.random.randn(h_curr, h_prec)
-        self.b = np.random.randn(h_curr, h_suiv)
-        return self.W, self.b
-    """ 
 class NeuralNetWork:
     # number of node in layer
     h0, h1, h2 = 2, 10, 1
@@ -87,15 +61,6 @@ class NeuralNetWork:
         self.NL1.forward_pass(X) #Compute Z1, A1
         self.NL2.forward_pass(self.NL1.Z) #Compute Z2 and A2
         
-    """      
-    def forward_pass(self, X, W1,W2, b1, b2):
-        
-        Z1 = W1.dot(X) + b1
-        A1 = sigmoid(Z1)
-        Z2 = W2.dot(A1) + b2
-        A2 = sigmoid(Z2)
-        return A2, Z2, A1, Z1
-    """ 
     
     def backward_pass(self, X, Y):
         # last layer
@@ -113,26 +78,7 @@ class NeuralNetWork:
         
         self.NL1.dW = ( self.NL1.dZ_dA.T * (self.NL2.dL_dA * self.NL2.dA_dZ) * self.NL1.dA_dZ  ) @ self.NL1.dZ_dW 
         self.NL1.db = ((self.NL2.dL_dA * self.NL2.dA_dZ) @ (self.NL1.dZ_dA.T * self.NL1.dA_dZ).T).T
-    """     
-    def backward_pass(X,Y, A2, Z2, A1, Z1, W1, W2, b1, b2):
 
-        # Your code here
-        dL_dA2 = (A2-Y)/(A2*(1-A2))
-        dA2_dZ2 = d_sigmoid(Z2)
-        dZ2_dW2 = A1.T
-
-        dW2 = ( dL_dA2 * dA2_dZ2  ) @ dZ2_dW2
-        db2 = dL_dA2 @ dA2_dZ2.T
-
-        dZ2_dA1 = W2
-        dA1_dZ1 = d_sigmoid(Z1)
-        dZ1_dW1 = X.T
-
-        dW1 = ( dZ2_dA1.T * (dL_dA2 * dA2_dZ2) * dA1_dZ1 ) @ dZ1_dW1
-        db1 = ((dL_dA2 * dA2_dZ2) @ (dZ2_dA1.T * dA1_dZ1).T).T 
-
-        return dW1, dW2, db1, db2
-    """ 
     def accuracy(self, y_pred, y):
 
         # Your code here
@@ -142,30 +88,20 @@ class NeuralNetWork:
         correct_predictions = (y_pred == y)
         return np.mean(correct_predictions)
 
-    """ 
-    def predict(X,W1,W2, b1, b2):
+    
+    def predict(self, X):
 
         # Your code here
-        A2, _, _, _ = forward_pass(X, W1, W2, b1, b2)
-        return A2
-    """ 
+        self.forward_pass(X)
+        return self.NL2.A
+    
     def update(self):
         self.NL1.W -= self.alpha * self.NL1.dW
         self.NL2.W -= self.alpha * self.NL2.dW
         
         self.NL1.b -= self.alpha * self.NL1.db
         self.NL2.b -= self.alpha * self.NL2.db
-    """     
-    def update(W1, W2, b1, b2,dW1, dW2, db1, db2, alpha ):
 
-        # Your code here
-        W1 -= alpha * dW1
-        W2 -= alpha * dW2
-        b1 -= alpha * db1
-        b2 -= alpha * db2
-
-        return W1, W2, b1, b2
-    """ 
     def loss(self, y_pred, Y):
         return  np.divide(-np.sum(Y * np.log(y_pred)+(1-Y)* np.log(1-y_pred)),Y.shape[1])
     
@@ -187,7 +123,7 @@ class NeuralNetWork:
             test_loss.append(self.loss(self.NL2.A, Y_test))
             
             ## plot boundary
-            if i % 1000 == 0:
+            if i % 50 == 0:
                 self.plot_decision_boundary()
                 
         return train_loss, test_loss
@@ -205,34 +141,6 @@ class NeuralNetWork:
         plt.show()
             
             
-            
-            
-    """   
-    def train ():
-        
-        alpha = 0.0001
-        W1, W2, b1, b2 = init_params()
-        n_epochs = 10000
-        train_loss = []
-        test_loss = []
-        for i in range(n_epochs):
-        ## forward pass
-        A2, Z2, A1, Z1 = forward_pass(X_train, W1, W2, b1, b2)
-        ## backward pass
-        dW1, dW2, db1, db2 = backward_pass(X_train, Y_train, A2, Z2, A1, Z1, W1, W2, b1, b2)
-        ## update parameters
-        W1, W2, b1, b2 = update(W1, W2, b1, b2, dW1, dW2, db1, db2, alpha )
-
-        ## save the train loss
-        train_loss.append(loss(A2, Y_train))
-        ## compute test loss
-        A2, Z2, A1, Z1 = forward_pass(X_test, W1, W2, b1, b2)
-        test_loss.append(loss(A2, Y_test))
-
-        ## plot boundary
-        if i %1000 == 0:
-            plot_decision_boundary(W1, W2, b1, b2)
-    """ 
 #   Action on data
 
 # generate data
@@ -277,10 +185,19 @@ if __name__ == "__main__":
     plt.show()
     
     
-    nn = NeuralNetWork()
+    nn = NeuralNetWork(0.1, 200)
     train_loss, test_loss = nn.train(X_train, X_test, Y_train, Y_test)
     
     ## plot train et test losses
     plt.plot(train_loss)
     plt.plot(test_loss)
+    plt.show()
+    
+    y_pred = nn.predict(X_train)
+    train_accuracy = nn.accuracy(y_pred, Y_train)
+    print ("train accuracy :", train_accuracy)
+
+    y_pred = nn.predict(X_test)
+    test_accuracy = nn.accuracy(y_pred, Y_test)
+    print ("test accuracy :", test_accuracy)
                 
